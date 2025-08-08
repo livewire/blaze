09f3c4aa1f07d653fc8270c3d9e941e855e06b4e
Caleb Porzio <calebporzio@gmail.com>
2025-08-07 21:20:55 -0400

    wip

--- src/BladeHacker.php
+++ src/BladeHacker.php
@@ -10,4 +10,16 @@ class BladeHacker
     {
         return Blade::render($template);
     }
+
+    public function componentPath($name): string
+    {
+        // Ingest a component name. For example:
+        // Blade components: <x-form.input> would be $name = 'form.input'
+        // Namespaced components: <x-pages::dashboard> would be $name = 'pages::dashboard'
+
+        // Then identify the source file path of that component and return it.
+
+        // Use as much of Laravel's codepath as possible to identify the component path.
+        // So that we don't maintain our own logic for this.
+    }
 }
