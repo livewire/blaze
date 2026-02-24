9b1dab6e35cb576919c4bd51c663ea9051f1b28d
prateekbhujel <prateekbhujelpb@gmail.com>
2026-02-24 13:35:29 +0545

    Fix Blaze compile context after optimize app swaps

--- tests/IntegrationTest.php
+++ tests/IntegrationTest.php
@@ -1,6 +1,8 @@
 <?php
 
+use Illuminate\Container\Container;
 use Illuminate\Support\Facades\Blade;
+use Illuminate\Support\Facades\Facade;
 
 test('ignores verbatim blocks', function () {
     $input = '@verbatim<x-input />@endverbatim';
@@ -18,6 +20,24 @@ test('ignores comments', function () {
     $input = '{{-- <x-input /> --}}';
 
     expect(Blade::render($input))->toBe('');
+});
+
+test('compiles using original app context when global container is swapped', function () {
+    $compiler = app('blade.compiler');
+    $originalApp = app();
+
+    $swapped = new Container;
+    Container::setInstance($swapped);
+    Facade::setFacadeApplication($swapped);
+
+    try {
+        $compiled = $compiler->compileString("@php \$name = 'Taylor'; @endphp\n<div>{{ \$name }}</div>");
+    } finally {
+        Container::setInstance($originalApp);
+        Facade::setFacadeApplication($originalApp);
+    }
+
+    expect($compiled)->not->toContain('@__raw_block_');
 });
 
 // TODO: Install PHPStan, which probably would have caught this.
