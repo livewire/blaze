8e8331427984bab2888c0b221730b787168a36b3
Filip Ganyicz <ganyicz.filip@gmail.com>
2026-03-01 19:58:02 +0100

    Add failing test

---
+++ tests/BladeServiceTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Blade;
+use Livewire\Blaze\BladeService;
+
+test('componentNameToPath', function ($callback, $prefix, $name, $path) {
+    $callback();
+
+    expect(app(BladeService::class)->componentNameToPath($prefix . $name))->toBe($path);
+})
+->with([
+    'default path' => [fn () => null, 'dummy.'],
+    'custom path' => [fn () => Blade::anonymousComponentPath(fixture_path('components/dummy')), ''],
+    'custom path with prefix' => [fn () => Blade::anonymousComponentPath(fixture_path('components/dummy'), 'dummy'), 'dummy::'],
+    'custom namespace' => [fn () => Blade::anonymousComponentNamespace('dummy'), 'dummy::'],
+])
+->with([
+    'exact file' => ['foo', fixture_path('components/dummy/foo.blade.php')],
+    'index file' => ['bar', fixture_path('components/dummy/bar/index.blade.php')],
+    'directory name' => ['baz', fixture_path('components/dummy/baz/baz.blade.php')],
+]);
