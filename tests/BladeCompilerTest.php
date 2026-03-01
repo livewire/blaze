efa5018f612c0b3ed439da8561949c9aa03297f6
Filip Ganyicz <ganyicz.filip@gmail.com>
2026-03-01 15:35:16 +0100

    Refactor

--- tests/BladeCompilerTest.php
+++ tests/BladeCompilerTest.php
@@ -10,12 +10,17 @@ use Livewire\Blaze\BlazeManager;
  * 
  * During `php artisan optimize`, Laravel bootstraps a fresh application instance
  * in `config:cache`, but later in `view:cache` it uses the original instance
- * to compile the views. Now, when we resolve blade compiler anywhere,
+ * to compile the views. Now, when we resolve the blade compiler anywhere,
  * it returns a different instance than the one compiling the view.
  * 
- * This only happens if blade engine is resolved during boot,
- * which packages like Sentry can trigger. Normally Laravel
- * would resolve the compiler from the new instance.
+ * This only happens if blade engine (which uses the compiler as a dependency)
+ * is resolved during boot by a package. Sentry does this to decorate it.
+ * Normally the compiler would get resolved from the new app instance.
+ * 
+ * Blaze heavily depends on the blade compiler and its internal state.
+ * Since we can't trust the container, these tests guarantee that
+ * we never use it to resolve the compiler. We must always use
+ * the instance that registered the precompilation hooks.
  */
 
 beforeEach(function () {
