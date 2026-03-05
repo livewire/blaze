dcb8d9c223c8c3f5d296060acb364bb0663beace
Filip Ganyicz <ganyicz.filip@gmail.com>
2026-03-05 20:27:05 +0100

    Fix custom namespaces (#100)
    * Add failing test
    
    * Refactor componentNameToPath
    
    * Move fixtures
    
    * Fix namespace test
    
    * Refactor
    
    * Fix test

---
+++ tests/BladeServiceTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Support\Facades\Blade;
+use Livewire\Blaze\BladeService;
+
+test('componentNameToPath', function ($callback, $prefix, $name, $path) {
+    $callback();
+
+    expect(app(BladeService::class)->componentNameToPath($prefix . $name))->toBe($path);
+})
+->with([
+    'default path' => [fn () => null, 'dummy.'],
+    'custom path' => [fn () => Blade::anonymousComponentPath(fixture_path('views/components/dummy')), ''],
+    'custom path with prefix' => [fn () => Blade::anonymousComponentPath(fixture_path('views/components/dummy'), 'dummy'), 'dummy::'],
+    'custom namespace' => [fn () => Blade::anonymousComponentNamespace('components.dummy', 'dummy'), 'dummy::'],
+])
+->with([
+    'exact file' => ['foo', fixture_path('views/components/dummy/foo.blade.php')],
+    'index file' => ['bar', fixture_path('views/components/dummy/bar/index.blade.php')],
+    'directory name' => ['baz', fixture_path('views/components/dummy/baz/baz.blade.php')],
+]);
