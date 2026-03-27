b0e36bb52edb60f332023c943a5e591b78ae62b3
Sander Muller <github@scode.nl>
2026-03-27 08:24:43 +0100

    optimize/kebab-case-perf

--- src/Runtime/BlazeRuntime.php
+++ src/Runtime/BlazeRuntime.php
@@ -140,12 +140,19 @@ class BlazeRuntime
         if ($attributes = $data['attributes'] ?? null) {
             unset($data['attributes']);
 
-            $this->dataStack[] = $this->normalizeKeys(array_merge($attributes->all(), $data));
-            $this->slotsStack[] = [];
-        } else {
-            $this->dataStack[] = $this->normalizeKeys($data);
-            $this->slotsStack[] = [];
+            $data = array_merge($attributes->all(), $data);
         }
+
+        foreach ($data as $key => $value) {
+            if (\str_contains($key, '-')) {
+                $data = $this->normalizeKeys($data);
+
+                break;
+            }
+        }
+
+        $this->dataStack[] = $data;
+        $this->slotsStack[] = [];
     }
 
     /**
