58153186ec5cae968d9038d9e1e84afadbbf152c
Filip Ganyicz <ganyicz.filip@gmail.com>
2026-06-08 11:43:11 +0200

    Refactor

--- src/Compiler/Compiler.php
+++ src/Compiler/Compiler.php
@@ -62,12 +62,8 @@ class Compiler
      */
     protected function shouldCompile(ComponentSource $source): bool
     {
-        if ($source->directives->blaze('compile') === false) {
-            return false;
-        }
-
         if ($source->directives->blaze()) {
-            return true;
+            return $source->directives->blaze('compile') ?? true;
         }
 
         return $this->config->shouldCompile($source->path)
