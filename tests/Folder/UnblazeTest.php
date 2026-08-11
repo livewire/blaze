d6259b53da8249e729a8613601eefb8718e1a832
Filip Ganyicz <ganyicz.filip@gmail.com>
2026-08-11 16:55:06 +0200

    Fix failing tests

--- tests/Folder/UnblazeTest.php
+++ tests/Folder/UnblazeTest.php
@@ -3,14 +3,13 @@
 use Livewire\Blaze\BladeRenderer;
 use Livewire\Blaze\BladeService;
 use Livewire\Blaze\Folder\Foldable;
-use Livewire\Blaze\Support\ComponentSource;
 use Livewire\Blaze\Parser\Parser;
 
 test('compiles unblaze blocks', function () {
     $input = '<x-foldable.input-unblaze name="address" />';
 
     $node = app(Parser::class)->parse($input)[0];
-    $foldable = new Foldable($node, new ComponentSource(fixture_path('views/components/foldable/input-unblaze.blade.php')), app(BladeRenderer::class), app(BladeService::class));
+    $foldable = new Foldable($node, fixture_path('views/components/foldable/input-unblaze.blade.php'), app(BladeRenderer::class), app(BladeService::class));
 
     expect($foldable->fold())->toEqualCollapsingWhitespace(
         sprintf('<input %s >', join('', [
@@ -26,7 +25,7 @@ test('compiles nested unblaze blocks', function () {
     $input = '<x-foldable.nested-input-unblaze />';
 
     $node = app(Parser::class)->parse($input)[0];
-    $foldable = new Foldable($node, new ComponentSource(fixture_path('views/components/foldable/nested-input-unblaze.blade.php')), app(BladeRenderer::class), app(BladeService::class));
+    $foldable = new Foldable($node, fixture_path('views/components/foldable/nested-input-unblaze.blade.php'), app(BladeRenderer::class), app(BladeService::class));
 
     expect($foldable->fold())->toEqualCollapsingWhitespace(
         sprintf('<div> <input %s ></div>', join('', [
@@ -42,7 +41,7 @@ test('folds dynamic attributes used inside unblaze directive', function () {
     $input = '<x-foldable.input-unblaze :name="$field" />';
 
     $node = app(Parser::class)->parse($input)[0];
-    $foldable = new Foldable($node, new ComponentSource(fixture_path('views/components/foldable/input-unblaze.blade.php')), app(BladeRenderer::class), app(BladeService::class));
+    $foldable = new Foldable($node, fixture_path('views/components/foldable/input-unblaze.blade.php'), app(BladeRenderer::class), app(BladeService::class));
 
     expect($foldable->fold())->toEqualCollapsingWhitespace(
         sprintf('<input %s >', join('', [
