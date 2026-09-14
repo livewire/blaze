223697ae9a525c4e14e2b7ceecefb549bb2bcc0e
Filip Ganyicz <ganyicz.filip@gmail.com>
2026-09-14 11:06:11 +0200

    Update benchmark

--- workbench/app/Providers/WorkbenchServiceProvider.php
+++ workbench/app/Providers/WorkbenchServiceProvider.php
@@ -5,6 +5,8 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Livewire\Blaze\Blaze;
 
+use function Orchestra\Testbench\workbench_path;
+
 class WorkbenchServiceProvider extends ServiceProvider
 {
     /**
@@ -20,6 +22,6 @@ class WorkbenchServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Blaze::optimize()->in(resource_path('views/components/bench/blaze'));
+        Blaze::optimize()->in(workbench_path('resources/views/components/bench/blaze'));
     }
 }
