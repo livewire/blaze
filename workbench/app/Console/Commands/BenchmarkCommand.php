e55fd32e743bb7ee5142da3d545c237919afe4b2
Filip Ganyicz <ganyicz.filip@gmail.com>
2026-09-15 11:27:16 +0200

    Add OPCache configuration warning to benchmark

--- workbench/app/Console/Commands/BenchmarkCommand.php
+++ workbench/app/Console/Commands/BenchmarkCommand.php
@@ -46,6 +46,8 @@ class BenchmarkCommand extends Command
             return Command::FAILURE;
         }
 
+        $this->checkOPCacheConfiguration();
+
         if ($attempts > 1) {
             return $this->runMultipleAttempts($attempts);
         }
@@ -73,6 +75,23 @@ class BenchmarkCommand extends Command
         }
 
         return Command::SUCCESS;
+    }
+
+    protected function checkOPCacheConfiguration(): void
+    {
+        $enabled = filter_var(ini_get('opcache.enable_cli'), FILTER_VALIDATE_BOOLEAN);
+        $protection = (int) ini_get('opcache.file_update_protection');
+
+        if ($enabled && $protection === 0) {
+            return;
+        }
+
+        $this->warn('OPcache is not configured for benchmarking, results will not reflect production performance.');
+        $this->warn('Add the following to your php.ini and rerun the benchmark:');
+        $this->newLine();
+        $this->line('opcache.enable_cli=1');
+        $this->line('opcache.file_update_protection=0');
+        $this->newLine();
     }
 
     protected function runBenchmark(): array
